63e20fea96045507b8a33241015a625aaf308a07
Ahmed Amamou <ahmedamamou30@gmail.com>
2024-03-15 00:05:13 +0100

    CSES challenge start

---
+++ CSES 150 Challenge/Introductory/Increasing_Array.cpp
@@ -0,0 +1,58 @@
+/*
+ ▄▀▀▄ ▄▄   ▄▀▀█▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀█▄▄
+█  █   ▄▀ ▐ ▄▀ ▀▄ █  █ ▀  █ █      █ █      █ █ ▄▀   █
+▐  █▄▄▄█    █▄▄▄█ ▐  █    █ █      █ █      █ ▐ █    █
+   █   █   ▄▀   █   █    █  ▀▄    ▄▀ ▀▄    ▄▀   █    █
+  ▄▀  ▄▀  █   ▄▀  ▄▀   ▄▀     ▀▀▀▀     ▀▀▀▀    ▄▀▄▄▄▄▀
+ █   █    ▐   ▐   █    █                      █     ▐
+ ▐   ▐            ▐    ▐                      ▐
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define F first
+#define double long double
+#define S second
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+using namespace std;
+
+const double EPS = 0.00000001;
+const ll MOD = 1e9 + 7;
+int gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * (b / gcd(a, b)); }
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll res = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < a[i - 1])
+        {
+            res += a[i - 1] - a[i];
+            a[i] = a[i - 1];
+        }
+    }
+    cout << res << endl;
+}
+
+signed main()
+{
+    FAST;
+    ll tt = 1;
+    // freopen("input.in", "r", stdin);
+    //   cin >> tt;
+    //   while (tt--)
+    solve();
+    return 0;
+}
---
+++ CSES 150 Challenge/Introductory/Repetitions.cpp
@@ -0,0 +1,58 @@
+/*
+ ▄▀▀▄ ▄▄   ▄▀▀█▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀█▄▄
+█  █   ▄▀ ▐ ▄▀ ▀▄ █  █ ▀  █ █      █ █      █ █ ▄▀   █
+▐  █▄▄▄█    █▄▄▄█ ▐  █    █ █      █ █      █ ▐ █    █
+   █   █   ▄▀   █   █    █  ▀▄    ▄▀ ▀▄    ▄▀   █    █
+  ▄▀  ▄▀  █   ▄▀  ▄▀   ▄▀     ▀▀▀▀     ▀▀▀▀    ▄▀▄▄▄▄▀
+ █   █    ▐   ▐   █    █                      █     ▐
+ ▐   ▐            ▐    ▐                      ▐
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define F first
+#define double long double
+#define S second
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+using namespace std;
+
+const double EPS = 0.00000001;
+const ll MOD = 1e9 + 7;
+int gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * (b / gcd(a, b)); }
+
+void solve()
+{
+    string s;
+    cin >> s;
+    ll ans = 1;
+    ll tmp = 1;
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            tmp++;
+            ans = max(ans, tmp);
+        }
+        else
+        {
+            tmp = 1;
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    FAST;
+    ll tt = 1;
+    // freopen("input.in", "r", stdin);
+    //   cin >> tt;
+    //   while (tt--)
+    solve();
+    return 0;
+}
