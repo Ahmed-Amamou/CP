d18d2c9c534d68f5e3d505ab3d179e5fccca07e7
Ahmed Amamou <ahmedamamou30@gmail.com>
2024-05-26 22:33:25 +0100

    Codeforces Round 948 (Div. 2)

---
+++ Codeforces/Contests/Codeforces Round 948 (Div. 2)/B_Binary_Colouring.cpp
@@ -0,0 +1,75 @@
+/*
+ ▄▀▀▄ ▄▄   ▄▀▀█▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀█▄▄
+█  █   ▄▀ ▐ ▄▀ ▀▄ █  █ ▀  █ █      █ █      █ █ ▄▀   █
+▐  █▄▄▄█    █▄▄▄█ ▐  █    █ █      █ █      █ ▐ █    █
+   █   █   ▄▀   █   █    █  ▀▄    ▄▀ ▀▄    ▄▀   █    █
+  ▄▀  ▄▀  █   ▄▀  ▄▀   ▄▀     ▀▀▀▀     ▀▀▀▀    ▄▀▄▄▄▄▀
+ █   █    ▐   ▐   █    █                      █     ▐
+ ▐   ▐            ▐    ▐                      ▐
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define F first
+#define double long double
+#define S second
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+using namespace std;
+
+const double EPS = 0.00000001;
+const ll MOD = 1e9 + 7;
+int gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * (b / gcd(a, b)); }
+
+void solve()
+{
+    int x;
+    cin >> x;
+
+    vector<int> a;
+    while (x)
+    {
+        if ((x % 2))
+        {
+            if ((x % 4) == 1)
+            {
+                x -= 1;
+                a.push_back(1);
+            }
+            else
+            {
+                x += 1;
+                a.push_back(-1);
+            }
+        }
+        else
+        {
+            a.push_back(0);
+        }
+
+        x = x / 2;
+    }
+
+    int n = a.size();
+    cout << n << "\n";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+signed main()
+{
+    FAST;
+    ll tt = 1;
+    // freopen("input.in", "r", stdin);
+    cin >> tt;
+    while (tt--)
+        solve();
+    return 0;
+}
