8126025f2269c44f569aa9e486907ea9deb251ee
Ahmed Amamou <aamamou@ats-digital.com>
2024-08-10 18:32:34 +0100

    back in business

---
+++ Codeforces/Practice/B_I_O_U.cpp
@@ -0,0 +1,59 @@
+/*
+ ▄▀▀▄ ▄▄   ▄▀▀█▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀█▄▄
+█  █   ▄▀ ▐ ▄▀ ▀▄ █  █ ▀  █ █      █ █      █ █ ▄▀   █
+▐  █▄▄▄█    █▄▄▄█ ▐  █    █ █      █ █      █ ▐ █    █
+   █   █   ▄▀   █   █    █  ▀▄    ▄▀ ▀▄    ▄▀   █    █
+  ▄▀  ▄▀  █   ▄▀  ▄▀   ▄▀     ▀▀▀▀     ▀▀▀▀    ▄▀▄▄▄▄▀
+ █   █    ▐   ▐   █    █                      █     ▐
+ ▐   ▐            ▐    ▐                      ▐
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define F first
+#define double long double
+#define S second
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+
+using namespace std;
+#define debug(x) cout << #x << ": " << x << endl;
+
+const double EPS = 0.00000001;
+const ll MOD = 1e9 + 7;
+int gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * (b / gcd(a, b)); }
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    int array[n + 1] = {0};
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        array[a] += c;
+        array[b] -= c;
+    }
+    ll sum = 0;
+    for (int i = 0; i < n + 1; i++)
+    {
+        sum += array[i] > 0 ? array[i] : 0;
+    }
+    cout << sum << endl;
+}
+
+signed main()
+{
+    FAST;
+    ll tt = 1;
+    // freopen("input.in", "r", stdin);
+    // cin >> tt;
+    // while (tt--)
+    solve();
+    return 0;
+}
