8e7be87c747f0bbcf29590dac65a18acf0df8bc7
Ahmed Amamou <ahmedamamou30@gmail.com>
2024-08-16 17:30:02 +0100

    nvm

---
+++ Codeforces/Practice/A_Snow_Footprints.cpp
@@ -0,0 +1,63 @@
+/*
+ ▄▀▀▄ ▄▄   ▄▀▀█▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀█▄▄
+█  █   ▄▀ ▐ ▄▀ ▀▄ █  █ ▀  █ █      █ █      █ █ ▄▀   █
+▐  █▄▄▄█    █▄▄▄█ ▐  █    █ █      █ █      █ ▐ █    █
+   █   █   ▄▀   █   █    █  ▀▄    ▄▀ ▀▄    ▄▀   █    █
+  ▄▀  ▄▀  █   ▄▀  ▄▀   ▄▀     ▀▀▀▀     ▀▀▀▀    ▄▀▄▄▄▄▀
+ █   █    ▐   ▐   █    █                      █     ▐
+ ▐   ▐            ▐    ▐                      ▐
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define F first
+#define double long double
+#define S second
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+
+using namespace std;
+
+const double EPS = 0.00000001;
+const ll MOD = 1e9 + 7;
+int gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * (b / gcd(a, b)); }
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int first_R = s.find_first_of('R') + 1;
+    int last_R = s.find_last_of('R') + 1;
+    int first_L = s.find_first_of('L') + 1;
+    int last_L = s.find_last_of('L') + 1;
+    // I know there's prob an easier way but fuck you :p
+    if (count(s.begin(), s.end(), 'L') == 0)
+    {
+        cout << first_R << " " << last_R + 1 << endl;
+    }
+    else if (count(s.begin(), s.end(), 'R') == 0)
+    {
+        cout << last_L << " " << first_L - 1 << endl;
+    }
+    else
+    {
+        cout << first_R << " " << last_R << endl;
+    }
+}
+
+signed main()
+{
+    FAST;
+    ll tt = 1;
+    // freopen("input.in", "r", stdin);
+    // cin >> tt;
+    // while (tt--)
+    solve();
+    return 0;
+}
