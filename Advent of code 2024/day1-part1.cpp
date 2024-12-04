ddf30c6abe4810212a0abbed2e43a65528e4519d
Ahmed Amamou <ahmedamamou30@gmail.com>
2024-12-04 14:13:56 +0100

    advent of code DAY1

---
+++ Advent of code 2024/day1-part1.cpp
@@ -0,0 +1,60 @@
+/*
+ ▄▀▀▄ ▄▄   ▄▀▀█▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀█▄▄
+█  █   ▄▀ ▐ ▄▀ ▀▄ █  █ ▀  █ █      █ █      █ █ ▄▀   █
+▐  █▄▄▄█    █▄▄▄█ ▐  █    █ █      █ █      █ ▐ █    █
+   █   █   ▄▀   █   █    █  ▀▄    ▄▀ ▀▄    ▄▀   █    █
+  ▄▀  ▄▀  █   ▄▀  ▄▀   ▄▀     ▀▀▀▀     ▀▀▀▀    ▄▀▄▄▄▄▀
+ █   █    ▐   ▐   █    █                      █     ▐
+ ▐   ▐            ▐    ▐                      ▐
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define F first
+#define double long double
+#define S second
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+
+using namespace std;
+#define debug(x) cout << #x << ": " << x << endl;
+
+const double EPS = 0.00000001;
+const ll MOD = 1e9 + 7;
+int gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * (b / gcd(a, b)); }
+
+void solve()
+{
+    vector<int> a, b;
+    while (true)
+    {
+        int x, y;
+        if (!(cin >> x >> y))
+            break;
+        a.push_back(x);
+        b.push_back(y);
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    int sum = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        sum += abs(a[i] - b[i]);
+    }
+    cout << sum << endl;
+}
+
+signed main()
+{
+    FAST;
+    ll tt = 1;
+    // freopen("input.in", "r", stdin);
+    // cin >> tt;
+    // while (tt--)
+        solve();
+    return 0;
+}
